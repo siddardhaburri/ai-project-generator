91b609730ebaf930a521b1c417f9b9ce0b4cd2fa
siddardhaburri <srinivasburri5384@gmail.com>
2026-03-21 21:54:13 +0530

    first

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+console.log("API URL:", process.env.REACT_APP_API_URL);
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
   timeout: 60000,
